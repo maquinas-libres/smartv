d0f3fcc8a6d43f159aeca35e6821ee77dcf0a750
b4zz4 <bazza@riseup.net>
2016-03-13 17:03:13 -0300

    instalacion

---
+++ instalar.sh
@@ -0,0 +1,10 @@
+sudo aptitude install -y lynx npm nodejs-legacy zenity transmission-gtk feh recode
+sudo npm -g install peerflix
+sudo pip install --upgrade youtube-dl
+
+cd /tmp/
+wget https://github.com/maquinas-libres/smartv/archive/master.zip
+unzip master.zip
+cd smartv-master
+
+sudo cp -r usr/local /usr/local
